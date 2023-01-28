4063f372a7fc3818730c8c54fd226ca9a0417a6f
luis <luisrym@gmail.com>
2023-01-28 06:44:31 +0100

    add test to execercise challege

--- challenge.js
+++ challenge.js
@@ -57,3 +57,24 @@ console.log(strictEquals(false, false)); // true
 console.log(strictEquals("Water", "oil")); // false
 console.log(strictEquals("Water", "Water")); // true
 console.log(strictEquals("", 0)); // false
+
+//TEST
+// import { strictEquals } from "./main.js";
+
+// test("strict equals of different values should be false", () => {
+//   const result = strictEquals(1, -1);
+//   expect(result).toBe(false);
+// });
+
+// test("strict equals of same values should be true", () => {
+//   expect(strictEquals("Water", "Water")).toBe(true);
+//   expect(strictEquals(10, 10)).toBe(true);
+// });
+
+// test("strict equals of 0 and -0 should be true", () => {
+//   expect(strictEquals(0, -0)).toBe(true);
+// });
+
+// test("strict equals of both NaN values should false", () => {
+//   expect(strictEquals(NaN, NaN)).toBe(false);
+// });
